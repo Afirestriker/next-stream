dce1175f346c77cd45afe8ea98a098e96b1cb344
Afirestriker <agrawalashish.in@gmail.com>
2024-03-31 16:29:33 +0530

    Updated Contact Form

--- src/app/components/ContactForm.js
+++ src/app/components/ContactForm.js
@@ -105,10 +105,11 @@ const ContactForm = () => {
                     <textarea id="message" name="message"
                            value={user.message}
                            rows={5}
+                           placeholder="Enter your message"
                            onChange={handleChange}
                            autoComplete="off"
                            required="required"
-                    > </textarea>
+                    />
                 </label>
             </div>
 
