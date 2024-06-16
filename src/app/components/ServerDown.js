d772d26d27798b86626a91b4085d1f323b3fced8
afirestriker <agrawalashish.in@gmail.com>
2024-06-16 14:28:45 +0530

    fix: (prod) missing alt attribute for image tag

--- src/app/components/ServerDown.js
+++ src/app/components/ServerDown.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
 
+const imageStyle = {
+    marginLeft: "auto",
+    marginRight: "auto"
+};
+
 const ServerDown = () => {
     return (
         <>
-            <Image loading="lazy" src="/dreamer.svg" width="700" height="500" style={{marginLeft: "auto", marginRight: "auto"}} />
+            <Image loading="lazy" alt="server down and maintenance" src="/dreamer.svg" width="700" height="500" style={imageStyle} />
             <p>"Looks like you've hit a pause button! ⏸️ Take this moment to unwind, grab a good book (check our footer for recommendations!), and we'll be back up and running soon. "</p>
         </>
     );
